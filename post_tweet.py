b56f09770f4330f632759bf13e4f813e3f870dd7
sallyconor <33449780+sallyconor@users.noreply.github.com>
2017-11-07 10:20:05 +0000

    Create post_tweet.py

---
+++ post_tweet.py
@@ -0,0 +1,61 @@
+import tweepy as tw
+
+def where_are_we():
+
+	with open("position.txt", 'r') as f:
+
+		counter = int(f.read())
+
+	return counter
+
+def reset_counter():
+
+	with open("position.txt", 'w') as f:
+
+		f.write('0')
+
+def update_position(counter, len_of_tweets):
+
+	with open("position.txt", 'w') as f:
+
+		f.write(str(int(counter % len_of_tweets)))
+
+def get_tweet(counter):
+
+	with open("tweets.txt", 'r') as f:
+
+		tweets = f.readlines()
+
+		
+	return tweets[counter].rstrip('\n'), len(tweets)
+
+
+def now_tweet(tweet):
+
+	with open("api.keys", 'r') as f:
+
+		keys = f.readlines()
+	
+
+	for i, key in enumerate(keys):
+		keys[i] = key.rstrip('\n')
+	
+
+	auth = tw.OAuthHandler(keys[0], keys[1])
+	auth.set_access_token(keys[2], keys[3])
+
+	api = tw.API(auth)
+
+
+	api.update_status(tweet)
+
+
+
+counter = where_are_we()
+
+tweet, n = get_tweet(counter)
+
+now_tweet(tweet)
+
+update_position(counter+1, n)
+
